8a4d5243d89576fb4e5464661fc71d3dde432b57
Alejandro Franco <Alejandro.franco@teaminternational.com>
2022-01-13 11:41:33 -0500

    Added able to check if the key is valid or not.

---
+++ substitution.c
@@ -0,0 +1,47 @@
+// this program encrypts a message given by the user using a 26 character key, replacing every letter of the alphabet with character of the key
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+int main(int argc, string argv[])
+{
+    if (argc != 2)
+    {
+        printf("\nYou did not enter a valid argument...\n\n");
+        return 1;
+    }
+
+    else if (strlen(argv[1]) != 26)
+    {
+        printf("\nKey must contain 26 characters.\n\n");
+        return 1;
+    }
+
+    int contador = 0;
+    while (contador < 26)
+    {
+        for (int i = contador + 1; i < 26; i++)
+        {
+            if (argv[1][contador] == argv[1][i])
+            {
+                printf("\nperro no repita letras\n\n");
+                return 1;
+            }
+        }
+        contador ++;    
+    }
+
+    for (int i = 0; i < strlen(argv[1]); i++)
+    { 
+        if (isalpha(argv[1][i]) == 0)
+        {
+            printf("\nKey must only contain letters\n\n");
+            return 1;
+        }
+    }
+    
+    string plaintext = get_string("Plaintext:  ");
+    printf("ciphertext: ");
+}
